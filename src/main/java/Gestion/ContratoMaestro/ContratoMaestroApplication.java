141bf2bfcc4e3e36bfd347d5fe8cb3fc5508e94c
FerValiente <fjvr333@gmail.com>
2024-05-31 12:19:03 -0400

    Archivos de Base

---
+++ src/main/java/Gestion/ContratoMaestro/ContratoMaestroApplication.java
@@ -0,0 +1,13 @@
+package Gestion.ContratoMaestro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratoMaestroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContratoMaestroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Gestion/ContratoMaestro/ContratoMaestroApplicationTests.java
@@ -0,0 +1,13 @@
+package Gestion.ContratoMaestro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratoMaestroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
